7885b6e28292af5a7e4f7a9fa46eda6c7adba7ad
Istvan Acs <istvan.acs89@gmail.com>
2018-11-23 13:13:43 +0000

    Create a svg list json

---
+++ src/client/components/Circle/SvgList.tsx
@@ -0,0 +1,22 @@
+export const svgs = [
+    {
+        id:1,
+        source: '',
+        text:'New Technologies'
+    },
+    {
+        id:2,
+        source: '',
+        text:'Performance'
+    },
+    {
+        id:3,
+        source: '',
+        text:'Design UI-UX'
+    },
+    {
+        id:4,
+        source: '',
+        text:'Best Practices'
+    }
+]
