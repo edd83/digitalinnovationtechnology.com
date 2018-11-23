18742c0c35489126936fb78c0e280ecf3a5698f3
Istvan Acs <istvan.acs89@gmail.com>
2018-11-23 13:13:04 +0000

    Create a circle list component

---
+++ src/client/components/CircleList/CircleList.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Circle from '../Circle/Circle';
+
+const styles = require('./CircleList.scss');
+
+export default class CircleList extends React.Component {
+    render() {
+        return (
+            <div className={styles.circleList}>
+            
+            </div>
+        )
+    }
+}
