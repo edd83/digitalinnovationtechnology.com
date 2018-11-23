bf26ee040c4becd81800c563f069a754f9b9761b
Istvan Acs <istvan.acs89@gmail.com>
2018-11-23 13:12:42 +0000

    Create a circle component

---
+++ src/client/components/Circle/Circle.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const styles = require('./Circle.scss');
+
+export default class Circle extends React.Component {
+    render() {
+        return (
+            <div className={styles.circle}>
+                
+            </div>
+        )
+    }
+}
