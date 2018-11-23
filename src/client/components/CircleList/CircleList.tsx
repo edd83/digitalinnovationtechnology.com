461c410fd91de7430b51a42e4fb45f65571999af
Istvan Acs <istvan.acs89@gmail.com>
2018-11-23 19:45:20 +0000

    Make the code beauty

--- src/client/components/CircleList/CircleList.tsx
+++ src/client/components/CircleList/CircleList.tsx
@@ -6,11 +6,13 @@ const styles = require('./CircleList.scss');
 
 const CircleList = ({ svgs }) => {
     const circleArray = svgs.map((circle, id) => {
-        return <Circle 
-            key={id} 
-            source={svgs[id].source} 
-            text={svgs[id].text}
-        />
+        return (
+            <Circle 
+                key={id} 
+                source={svgs[id].source} 
+                text={svgs[id].text}
+            />
+        )
     })
     return (
         <div className={styles.circleList}>
