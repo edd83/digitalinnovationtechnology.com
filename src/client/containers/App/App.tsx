add69d8aad1b10eb641a3e4074ca4bb81136b27b
Istvan Acs <istvan.acs89@gmail.com>
2018-11-23 19:05:30 +0000

    Add components to the app WIP

--- src/client/containers/App/App.tsx
+++ src/client/containers/App/App.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import {connect} from 'react-redux';
 import ImageHome from '../../components/ImageHome/ImageHome';
 import CircleList from '../../components/CircleList/CircleList';
+import Circle from '../../components/Circle/Circle';
 import LayerComponent from '../../components/LayerComponent/LayerComponent';
+import { svgs } from '../../components/Circle/SvgList'
 
 const styles = require('./App.scss');
 
@@ -11,7 +13,11 @@ export class AppLayout extends React.Component<{}, {}> {
     return (
       <div>
         <ImageHome />
-        <CircleList />
+        <Circle source={svgs[0].source} text={svgs[0].text}/>
+        <Circle source={svgs[1].source} text={svgs[1].text}/>
+        <Circle source={svgs[2].source} text={svgs[2].text}/>
+        <Circle source={svgs[3].source} text={svgs[3].text}/>
+        {/* <CircleList /> */}
         <LayerComponent idx={1} component={ImageHome} />
       </div>
     );
